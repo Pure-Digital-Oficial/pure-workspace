bb666ac0c0ee89e35b8cc0ce5b3c8ad47d08a1d5
Pura Fome <samuelpe06062000@gmail.com>
2025-04-11 19:09:22 -0300

    feat: create system user data access

--- libs/data-access/src/lib/infra/repositories/postgres/user/index.ts
+++ libs/data-access/src/lib/infra/repositories/postgres/user/index.ts
@@ -3,3 +3,4 @@ export * from './find-user-by-email.repository';
 export * from './find-user-by-id.repository';
 export * from './find-user-by-nickname.repository';
 export * from './find-user-in-app.repository';
+export * from './create-system-user.repository';
