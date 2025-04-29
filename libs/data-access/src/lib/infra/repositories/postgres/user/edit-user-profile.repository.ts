3b8cace09623db1ff5613771afccce1dc597453b
Erick Celestino <erickcelestimo@gmail.com>
2025-04-29 17:58:18 -0300

    feat: add edit user profile repository implementation

---
+++ libs/data-access/src/lib/infra/repositories/postgres/user/edit-user-profile.repository.ts
@@ -0,0 +1,43 @@
+import {
+  EditUserProfileDto,
+  EditUserProfileRepository,
+} from '@pure-workspace/domain';
+import { PrismaGeneralService } from '../../../../application';
+import { Inject } from '@nestjs/common';
+
+export class EditUserProfileRepositoryImpl
+  implements EditUserProfileRepository
+{
+  constructor(
+    @Inject('PrismaService') private prismaService: PrismaGeneralService
+  ) {}
+  async edit(input: EditUserProfileDto): Promise<string> {
+    const { id, name, birthDate, picture, email } = input;
+
+    const editedUser = await this.prismaService['user'].update({
+      where: {
+        id,
+      },
+      data: {
+        name,
+        ...(Object.keys({ birthDate }).length > 1
+          ? { birth_date: birthDate }
+          : {}),
+        ...(picture !== null ? { picture } : {}),
+        auth: {
+          update: {
+            where: {
+              user_id: id,
+            },
+            data: {
+              email,
+            },
+          },
+        },
+        updated_at: new Date(),
+      },
+    });
+
+    return editedUser.id;
+  }
+}
