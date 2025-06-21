255d96595a9afebdfff5163fcce3bdc558ebd9af
Erick Celestino <erickcelestimo@gmail.com>
2025-06-21 13:29:32 -0300

    feat: add delete trigger and find user in trigger repository implementation

---
+++ libs/data-access/src/lib/infra/repositories/postgres/marketing/trigger/delete-trigger.repository.ts
@@ -0,0 +1,22 @@
+import { Inject } from '@nestjs/common';
+import { PrismaService } from 'nestjs-prisma';
+import {
+  DeleteTriggerDto,
+  DeleteTriggerRepository,
+} from '@pure-workspace/domain';
+
+export class DeleteTriggerRepositoryImpl implements DeleteTriggerRepository {
+  constructor(@Inject('PrismaService') private prismaService: PrismaService) {}
+  async delete(input: DeleteTriggerDto): Promise<string> {
+    const updatedTrigger = await this.prismaService['trigger'].update({
+      where: {
+        id: input.id,
+      },
+      data: {
+        status: 'INACTIVE',
+      },
+    });
+
+    return updatedTrigger.id;
+  }
+}
--- libs/data-access/src/lib/infra/repositories/postgres/marketing/trigger/index.ts
+++ libs/data-access/src/lib/infra/repositories/postgres/marketing/trigger/index.ts
@@ -1,4 +1,6 @@
 export * from './create-trigger.repository';
+export * from './delete-trigger.repository';
 export * from './find-trigger-by-content.repository';
 export * from './find-trigger-by-name.repository';
+export * from './find-user-in-trigger.repository';
 export * from './list-triggers.repository';
