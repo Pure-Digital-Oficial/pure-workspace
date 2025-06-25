8aee30eededd303ad7c1b0f261bf76de70468709
Erick Celestino <erickcelestimo@gmail.com>
2025-06-24 22:28:24 -0300

    feat: add list targets repository implementation

--- libs/data-access/src/lib/infra/repositories/postgres/marketing/target/index.ts
+++ libs/data-access/src/lib/infra/repositories/postgres/marketing/target/index.ts
@@ -1,2 +1,3 @@
 export * from './create-unique-target.repository';
 export * from './find-target-by-content.repository';
+export * from './list-targets.repository';
--- libs/domain/src/lib/dtos/response/prisma/index.ts
+++ libs/domain/src/lib/dtos/response/prisma/index.ts
@@ -1,2 +1,3 @@
+export * from './target-prisma.response.dto';
 export * from './trigger-prisma.response.dto';
 export * from './user-prisma.response.dto';
