ae4552d1afa228bfd583f58f2bbb80ef994b54e9
Erick Celestino <erickcelestimo@gmail.com>
2025-06-28 13:48:06 -0300

    feat: add edit target repository implementation

--- libs/data-access/src/lib/infra/repositories/postgres/marketing/target/index.ts
+++ libs/data-access/src/lib/infra/repositories/postgres/marketing/target/index.ts
@@ -1,5 +1,6 @@
 export * from './create-unique-target.repository';
 export * from './delete-target.repository';
+export * from './edit-target.repository';
 export * from './find-target-by-content.repository';
 export * from './find-user-in-target.repository';
 export * from './list-targets.repository';
