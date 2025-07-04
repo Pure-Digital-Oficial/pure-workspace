43d62fbb2f960c77e4cef15c0add1f6218a2a854
Erick Celestino <erickcelestimo@gmail.com>
2025-07-03 21:55:18 -0300

    feat: add find user in shot model and delete shot model repository implementation

---
+++ libs/data-access/src/lib/infra/repositories/postgres/marketing/shot/shot-model/find-user-in-shot-model.repository.ts
@@ -0,0 +1,22 @@
+import { Inject } from '@nestjs/common';
+import {
+  FindUserInShotModelDto,
+  FindUserInShotModelRepository,
+} from '@pure-workspace/domain';
+import { PrismaService } from 'nestjs-prisma';
+
+export class FindUserInShotModelRepositoryImpl
+  implements FindUserInShotModelRepository
+{
+  constructor(@Inject('PrismaService') private prismaService: PrismaService) {}
+  async find(input: FindUserInShotModelDto): Promise<string> {
+    const findedShotModel = await this.prismaService['shot_model'].findUnique({
+      where: {
+        id: input.shotModelId,
+        user_id: input.loggedUserId,
+      },
+    });
+
+    return findedShotModel?.id ?? '';
+  }
+}
