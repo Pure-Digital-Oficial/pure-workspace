32ad6ea706fb5d9a99fe261ac05ea7df0d8d025c
Erick Celestino <erickcelestimo@gmail.com>
2025-07-05 13:56:33 -0300

    test: ensure user of inputs and business rules in the EditShotModel class

--- libs/domain/src/test/repositories/marketing/shot/shot-model/index.ts
+++ libs/domain/src/test/repositories/marketing/shot/shot-model/index.ts
@@ -1,5 +1,6 @@
 export * from './create-shot-model.mock';
 export * from './delete-shot-model.mock';
+export * from './edit-shot-model.mock';
 export * from './find-shot-model-by-subject.mock';
 export * from './find-shot-model-by-title.mock';
 export * from './find-user-in-shot-model.mock';
