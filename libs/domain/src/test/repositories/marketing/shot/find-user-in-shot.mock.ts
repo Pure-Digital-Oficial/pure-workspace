c1de354e8b443f7f7a58530b6bdc901b50e7137d
Erick Celestino <erickcelestimo@gmail.com>
2025-07-06 12:19:03 -0300

    test: ensure user inputs and business rules in the DeleteShot use case

---
+++ libs/domain/src/test/repositories/marketing/shot/find-user-in-shot.mock.ts
@@ -0,0 +1,13 @@
+import {
+  FindUserInShotDto,
+  FindUserInShotRepository,
+} from '../../../../../src';
+import { ShotMock } from '../../../entities';
+
+export class FindUserInShotRepositoryMock implements FindUserInShotRepository {
+  inputMock = {} as FindUserInShotDto;
+  async find(input: FindUserInShotDto): Promise<string> {
+    this.inputMock = input;
+    return ShotMock.id;
+  }
+}
