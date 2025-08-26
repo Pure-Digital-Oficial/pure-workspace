425bff617b9d143b40a99962d7f14304e01e2ec9
Erick Celestino <erickcelestimo@gmail.com>
2025-08-26 14:38:12 -0300

    chore: add feature angular library

--- apps/web/angular/a-web-pure-digital/src/app/app.routes.ts
+++ apps/web/angular/a-web-pure-digital/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('@pure-workspace/feature-angular').then(
+        (c) => c.FeatureAngularComponent
+      ),
+  },
+];
---
+++ libs/feature-angular/src/lib/feature-angular/feature-angular.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-feature-angular',
+  imports: [CommonModule],
+  templateUrl: './feature-angular.component.html',
+  styleUrl: './feature-angular.component.scss',
+})
+export class FeatureAngularComponent {}
