b1dd6fecc47ac703fb624167c533e6ad7a593eb6
Erick Celestino <erickcelestimo@gmail.com>
2025-10-26 23:17:33 -0300

    feat: add menu component in the list item transaction controls component

--- libs/feature-angular/src/lib/components/controls/transactions/list-item-transaction/list-item-transaction-controls.component.ts
+++ libs/feature-angular/src/lib/components/controls/transactions/list-item-transaction/list-item-transaction-controls.component.ts
@@ -1,11 +1,21 @@
 import { CurrencyPipe, DatePipe } from '@angular/common';
 import { Component, computed, input } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
+import { MatIcon } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 import { TransactionResponseDto } from '@pure-workspace/domain';
 
 @Component({
   selector: 'lib-list-item-transaction-controls',
-  imports: [CurrencyPipe, DatePipe, MatListModule],
+  imports: [
+    CurrencyPipe,
+    DatePipe,
+    MatListModule,
+    MatButtonModule,
+    MatIcon,
+    MatMenuModule,
+  ],
   templateUrl: './list-item-transaction-controls.component.html',
   styleUrl: './list-item-transaction-controls.component.scss',
 })
