0af2df30046a8ca7b0741c8079a4c3ae4422b50a
Erick Celestino <erickcelestimo@gmail.com>
2025-10-26 22:51:12 -0300

    chore: add pt br locale in the pure digital app

--- apps/web/angular/a-web-pure-digital/src/app/app.config.ts
+++ apps/web/angular/a-web-pure-digital/src/app/app.config.ts
@@ -1,4 +1,8 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideZoneChangeDetection,
+  LOCALE_ID,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import {
@@ -7,6 +11,10 @@ import {
   withInterceptorsFromDi,
 } from '@angular/common/http';
 import { AuthInterceptor } from '@pure-workspace/feature-angular';
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localePt, 'pt-BR');
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,5 +22,6 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withInterceptorsFromDi()),
     provideRouter(appRoutes),
     provideHttpClient(withInterceptors([AuthInterceptor])),
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
   ],
 };
