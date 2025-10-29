ceed1333c763a6ebdfc3913f95ed77c3767d49b2
Erick Celestino <erickcelestimo@gmail.com>
2025-10-29 13:51:18 -0300

    feat: add zod route validation and auth guard

--- apps/server/server-pure-finance/src/app/transaction/edit-transaction/edit-transaction.controller.ts
+++ apps/server/server-pure-finance/src/app/transaction/edit-transaction/edit-transaction.controller.ts
@@ -1,12 +1,34 @@
-import { Body, Controller, Param, Put, Query } from '@nestjs/common';
-import { EditTransactionDto, ErrorMessageResult } from '@pure-workspace/domain';
+import {
+  Body,
+  Controller,
+  Param,
+  Put,
+  Query,
+  UseGuards,
+  UsePipes,
+} from '@nestjs/common';
+import {
+  EditTransactionDto,
+  ErrorMessageResult,
+  transactionBodySchema,
+  userIdQuerySchema,
+} from '@pure-workspace/domain';
+import { JwtAuthGuard } from '@pure-workspace/data-access';
 import { EditTransactionService } from './edit-transaction.service';
+import { ZodValidationPipe } from '../../pipes';
 
 @Controller('transaction/edit-transaction')
 export class EditTransactionController {
   constructor(private editTransactionService: EditTransactionService) {}
 
   @Put(':id')
+  @UseGuards(JwtAuthGuard)
+  @UsePipes(
+    new ZodValidationPipe({
+      query: userIdQuerySchema,
+      body: transactionBodySchema,
+    })
+  )
   async edit(
     @Param('id') id: string,
     @Query() query: { userId: string },
