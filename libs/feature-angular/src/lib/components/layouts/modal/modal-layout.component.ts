745bb500c48f9be9521135df3ba1624abd9a89bc
Erick Celestino <erickcelestimo@gmail.com>
2025-11-01 14:07:30 -0300

    feat: add button icon and inplement in the modal layout component

--- libs/feature-angular/src/lib/components/layouts/modal/modal-layout.component.ts
+++ libs/feature-angular/src/lib/components/layouts/modal/modal-layout.component.ts
@@ -1,13 +1,13 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
-import { MatIcon } from '@angular/material/icon';
+import { DefaultComponent } from '../../buttons';
 
 @Component({
   selector: 'lib-modal-layout',
   templateUrl: 'modal-layout.component.html',
   styleUrl: 'modal-layout.component.scss',
-  imports: [MatDialogModule, MatButtonModule, MatIcon],
+  imports: [MatDialogModule, MatButtonModule, DefaultComponent],
 })
 export class ModalLayoutComponent {
   @Input() title = 'Modal Title';
