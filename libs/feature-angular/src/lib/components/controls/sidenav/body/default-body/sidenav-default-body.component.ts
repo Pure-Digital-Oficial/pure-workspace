8d6bf788446638f6b801d958339bd1520c32b9ab
Erick Celestino <erickcelestimo@gmail.com>
2025-11-02 22:22:50 -0300

    refactor: ajust padding in the sidenav default body component

--- libs/feature-angular/src/lib/components/controls/sidenav/body/default-body/sidenav-default-body.component.ts
+++ libs/feature-angular/src/lib/components/controls/sidenav/body/default-body/sidenav-default-body.component.ts
@@ -5,9 +5,9 @@ import { MatDrawer } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTreeModule } from '@angular/material/tree';
 import { SidenavItem } from '@pure-workspace/domain';
-import { UserPhotoControlsComponent } from '../../../users';
 import { MatDivider } from '@angular/material/divider';
-import { MatButtonModule } from '@angular/material/button';
+import { UserPhotoControlsComponent } from '../../../users';
+import { DefaultButtonIconComponent } from '../../../../buttons';
 
 @Component({
   selector: 'lib-sidenav-default-body',
@@ -15,11 +15,11 @@ import { MatButtonModule } from '@angular/material/button';
   imports: [
     CommonModule,
     RouterModule,
-    MatIconModule,
-    MatButtonModule,
     MatTreeModule,
-    UserPhotoControlsComponent,
+    MatIconModule,
     MatDivider,
+    UserPhotoControlsComponent,
+    DefaultButtonIconComponent,
   ],
   templateUrl: './sidenav-default-body.component.html',
   styleUrl: './sidenav-default-body.component.scss',
@@ -29,4 +29,7 @@ export class SidenavDefaultBodyComponent {
   @Input() drawer: MatDrawer = {} as MatDrawer;
 
   getChildren = (node: SidenavItem) => node.children ?? [];
+
+  hasChild = (_: number, node: SidenavItem) =>
+    !!node.children && node.children.length > 0;
 }
