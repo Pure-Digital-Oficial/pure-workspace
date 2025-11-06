ad58db80ebd9d04278d80c12cd9caa44f01dc051
Erick Celestino <erickcelestimo@gmail.com>
2025-11-05 21:31:36 -0300

    feat: add list fixed gains repository implementation

---
+++ libs/domain/src/lib/dtos/response/prisma/fixed-gain.prisma.response.dto.ts
@@ -0,0 +1,15 @@
+import { FixedGainType, GeneralStatus } from '../../../types';
+
+export interface FixedGainPrismaResponseDto {
+  id: string;
+  name: string;
+  value: number;
+  status: GeneralStatus;
+  day_of_receipt: number;
+  frequency: FixedGainType;
+  created_at: Date;
+  updated_at: Date;
+  user: {
+    nickname: string;
+  };
+}
