630287d7aee4171b36c24fa40654ad28721dbacf
PuraPreguiça <erickcelestimo@gmail.com>
2025-11-27 22:17:54 -0300

    test: ensure user inputs and business rules in the delete budget with category transaction use case

--- libs/domain/src/test/repositories/finance/budget-with-category-transaction/index.ts
+++ libs/domain/src/test/repositories/finance/budget-with-category-transaction/index.ts
@@ -1,3 +1,4 @@
 export * from './create-budget-with-category-transaction.mock';
+export * from './delete-budget-with-category-transaction.mock';
 export * from './find-budget-with-category-transaction-by-ids.mock';
 export * from './list-budget-with-category-transactions.mock';
---
+++ libs/domain/src/test/use-cases/finance/budget-with-category-transaction/delete-budget-with-category-transaction.spec.ts
@@ -0,0 +1,180 @@
+import {
+  BudgetResponseDto,
+  CategoryTransactionResponseDto,
+  DeleteBudgetWithCategoryTransactionDto,
+  UserResponseDto,
+} from '@/dtos';
+import {
+  DeleteBudgetWithCategoryTransactionRepository,
+  FindBudgetByIdRepository,
+  FindBudgetWithCategoryTransactionByIdsRepository,
+  FindCategoryTransactionByIdRepository,
+  FindUserByIdRepository,
+} from '@/repositories';
+import { DeleteBudgetWithCategoryTransaction } from '@/use-cases';
+import {
+  BudgetMock,
+  BudgetWithCategoryTransactionMock,
+  CategoryTransactionMock,
+  UserMock,
+} from '@/test/entities';
+import {
+  DeleteBudgetWithCategoryTransactionRepositoryMock,
+  FindBudgetByIdRepositoryMock,
+  FindBudgetWithCategoryTransactionByIdsRepositoryMock,
+  FindCategoryTransactionByIdRepositoryMock,
+  FindUserByIdRepositoryMock,
+} from '@/test/repositories';
+import {
+  EntityAlreadyExists,
+  EntityNotDeleted,
+  EntityNotEmpty,
+  EntityNotExists,
+} from '@/errors';
+
+export interface SutTypes {
+  sut: DeleteBudgetWithCategoryTransaction;
+  deleteBudgetWithCategoryTransactionDto: DeleteBudgetWithCategoryTransactionDto;
+  findUserByIdRepository: FindUserByIdRepository;
+  findCategoryTransactionByIdRepository: FindCategoryTransactionByIdRepository;
+  findBudgetByIdRepository: FindBudgetByIdRepository;
+  findBudgetWithCategoryTransactionByIdsRepository: FindBudgetWithCategoryTransactionByIdsRepository;
+  deleteBudgetWithCategoryTransactionRepository: DeleteBudgetWithCategoryTransactionRepository;
+}
+
+const makeSut = (): SutTypes => {
+  const findUserByIdRepository = new FindUserByIdRepositoryMock();
+  const findCategoryTransactionByIdRepository =
+    new FindCategoryTransactionByIdRepositoryMock();
+  const findBudgetByIdRepository = new FindBudgetByIdRepositoryMock();
+  const findBudgetWithCategoryTransactionByIdsRepository =
+    new FindBudgetWithCategoryTransactionByIdsRepositoryMock();
+  const deleteBudgetWithCategoryTransactionRepository =
+    new DeleteBudgetWithCategoryTransactionRepositoryMock();
+
+  const deleteBudgetWithCategoryTransactionDto: DeleteBudgetWithCategoryTransactionDto =
+    {
+      budgetId: BudgetMock.id,
+      categoryTransactionId: CategoryTransactionMock.id,
+      loggedUserId: UserMock.id,
+    };
+
+  const sut = new DeleteBudgetWithCategoryTransaction(
+    findUserByIdRepository,
+    findCategoryTransactionByIdRepository,
+    findBudgetByIdRepository,
+    findBudgetWithCategoryTransactionByIdsRepository,
+    deleteBudgetWithCategoryTransactionRepository
+  );
+
+  return {
+    sut,
+    deleteBudgetWithCategoryTransactionDto,
+    findUserByIdRepository,
+    findCategoryTransactionByIdRepository,
+    findBudgetByIdRepository,
+    findBudgetWithCategoryTransactionByIdsRepository,
+    deleteBudgetWithCategoryTransactionRepository,
+  };
+};
+
+describe('DeleteBudgetWithCategoryTransaction', () => {
+  it('should return budget with category transaction ID when pass correct deleteBudgetWithCategoryTransactionDto object', async () => {
+    const { deleteBudgetWithCategoryTransactionDto, sut } = makeSut();
+
+    const result = await sut.execute(deleteBudgetWithCategoryTransactionDto);
+
+    expect(result.isLeft()).toBeFalsy();
+    expect(result.isRight()).toBeTruthy();
+    expect(result.value).toStrictEqual(BudgetWithCategoryTransactionMock.id);
+  });
+  it('should return EntityNotEmpty when pass empty loggedUserId in deleteBudgetWithCategoryTransactionDto object', async () => {
+    const { deleteBudgetWithCategoryTransactionDto, sut } = makeSut();
+    deleteBudgetWithCategoryTransactionDto.loggedUserId = '';
+    const result = await sut.execute(deleteBudgetWithCategoryTransactionDto);
+
+    expect(result.isLeft()).toBeTruthy();
+    expect(result.isRight()).toBeFalsy();
+    expect(result.value).toBeInstanceOf(EntityNotEmpty);
+  });
+
+  it('should return EntityNotEmpty when pass empty budgetId in deleteBudgetWithCategoryTransactionDto object', async () => {
+    const { deleteBudgetWithCategoryTransactionDto, sut } = makeSut();
+    deleteBudgetWithCategoryTransactionDto.budgetId = '';
+    const result = await sut.execute(deleteBudgetWithCategoryTransactionDto);
+
+    expect(result.isLeft()).toBeTruthy();
+    expect(result.isRight()).toBeFalsy();
+    expect(result.value).toBeInstanceOf(EntityNotEmpty);
+  });
+
+  it('should return EntityNotEmpty when pass empty categoryTransactionId in deleteBudgetWithCategoryTransactionDto object', async () => {
+    const { deleteBudgetWithCategoryTransactionDto, sut } = makeSut();
+    deleteBudgetWithCategoryTransactionDto.categoryTransactionId = '';
+    const result = await sut.execute(deleteBudgetWithCategoryTransactionDto);
+
+    expect(result.isLeft()).toBeTruthy();
+    expect(result.isRight()).toBeFalsy();
+    expect(result.value).toBeInstanceOf(EntityNotEmpty);
+  });
+
+  it('should return EntityNotExists when pass incorrect logged User ID in deleteBudgetWithCategoryTransactionDto object', async () => {
+    const { deleteBudgetWithCategoryTransactionDto, sut } = makeSut();
+    jest
+      .spyOn(sut['findUserByIdRepository'], 'find')
+      .mockResolvedValueOnce({} as UserResponseDto);
+    const result = await sut.execute(deleteBudgetWithCategoryTransactionDto);
+
+    expect(result.isLeft()).toBeTruthy();
+    expect(result.isRight()).toBeFalsy();
+    expect(result.value).toBeInstanceOf(EntityNotExists);
+  });
+
+  it('should return EntityNotExists when pass incorrect category transaction ID in deleteBudgetWithCategoryTransactionDto object', async () => {
+    const { deleteBudgetWithCategoryTransactionDto, sut } = makeSut();
+    jest
+      .spyOn(sut['findCategoryTransactionByIdRepository'], 'find')
+      .mockResolvedValueOnce({} as CategoryTransactionResponseDto);
+    const result = await sut.execute(deleteBudgetWithCategoryTransactionDto);
+
+    expect(result.isLeft()).toBeTruthy();
+    expect(result.isRight()).toBeFalsy();
+    expect(result.value).toBeInstanceOf(EntityNotExists);
+  });
+
+  it('should return EntityNotExists when pass incorrect budget ID in deleteBudgetWithCategoryTransactionDto object', async () => {
+    const { deleteBudgetWithCategoryTransactionDto, sut } = makeSut();
+    jest
+      .spyOn(sut['findBudgetByIdRepository'], 'find')
+      .mockResolvedValueOnce({} as BudgetResponseDto);
+    const result = await sut.execute(deleteBudgetWithCategoryTransactionDto);
+
+    expect(result.isLeft()).toBeTruthy();
+    expect(result.isRight()).toBeFalsy();
+    expect(result.value).toBeInstanceOf(EntityNotExists);
+  });
+
+  it('should return EntityAlreadyExists when exist the budget with category transaction in the database', async () => {
+    const { deleteBudgetWithCategoryTransactionDto, sut } = makeSut();
+    jest
+      .spyOn(sut['findBudgetWithCategoryTransactionByIdsRepository'], 'find')
+      .mockResolvedValueOnce(BudgetWithCategoryTransactionMock.id);
+    const result = await sut.execute(deleteBudgetWithCategoryTransactionDto);
+
+    expect(result.isLeft()).toBeTruthy();
+    expect(result.isRight()).toBeFalsy();
+    expect(result.value).toBeInstanceOf(EntityAlreadyExists);
+  });
+
+  it('should return EntityNotDeleted when not deleted the budget with category transaction in the database', async () => {
+    const { deleteBudgetWithCategoryTransactionDto, sut } = makeSut();
+    jest
+      .spyOn(sut['deleteBudgetWithCategoryTransactionRepository'], 'delete')
+      .mockResolvedValueOnce('');
+    const result = await sut.execute(deleteBudgetWithCategoryTransactionDto);
+
+    expect(result.isLeft()).toBeTruthy();
+    expect(result.isRight()).toBeFalsy();
+    expect(result.value).toBeInstanceOf(EntityNotDeleted);
+  });
+});
