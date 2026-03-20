961e4994467b16e9764d0fe188c148a4ee70cef3
PuraPreguiça <erickcelestimo@gmail.com>
2026-03-20 15:34:06 -0300

    feat: add find customer by id and edit customer repository implementation

---
+++ libs/data-access/src/lib/infra/repositories/postgres/marketing/customer/find-customer-by-id.repository.ts
@@ -0,0 +1,46 @@
+import { Inject } from '@nestjs/common';
+import { PrismaService } from 'nestjs-prisma';
+import {
+  CustomerResponseDto,
+  FindCustomerByIdRepository,
+} from '@pure-workspace/domain';
+
+export class FindCustomerByIdRepositoryImpl
+  implements FindCustomerByIdRepository
+{
+  constructor(@Inject('PrismaService') private prismaService: PrismaService) {}
+  async find(id: string): Promise<CustomerResponseDto> {
+    const findedCustomer = await this.prismaService['customer'].findFirst({
+      where: {
+        id,
+      },
+      select: {
+        id: true,
+        external_id: true,
+        name: true,
+        language: true,
+        frequency_payment: true,
+        user: {
+          select: {
+            nickname: true,
+          },
+        },
+        status: true,
+        updated_at: true,
+        created_at: true,
+      },
+    });
+
+    return {
+      id: findedCustomer?.id ?? '',
+      externalId: findedCustomer?.external_id ?? '',
+      name: findedCustomer?.name ?? '',
+      language: findedCustomer?.language ?? '',
+      status: findedCustomer?.status ?? '',
+      updatedAt: findedCustomer?.updated_at ?? new Date(),
+      createdAt: findedCustomer?.created_at ?? new Date(),
+      createdBy: findedCustomer?.user.nickname ?? '',
+      frequencyPayment: findedCustomer?.frequency_payment ?? '',
+    };
+  }
+}
--- libs/data-access/src/lib/infra/repositories/postgres/marketing/customer/index.ts
+++ libs/data-access/src/lib/infra/repositories/postgres/marketing/customer/index.ts
@@ -1,2 +1,4 @@
 export * from './create-customer.repository';
+export * from './edit-customer-status.repository';
 export * from './find-customer-by-external-id.repository';
+export * from './find-customer-by-id.repository';
